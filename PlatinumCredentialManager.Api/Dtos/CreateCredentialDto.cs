9b14830b8b07aaf200045265d304ab76972ede6d
richardnguyen <richard.nguyen@coredata.ca>
2026-06-04 15:30:50 -0600

    Made sure variables are following CamelCase conentions

--- PlatinumCredentialManager.Api/Dtos/CreateCredentialDto.cs
+++ PlatinumCredentialManager.Api/Dtos/CreateCredentialDto.cs
@@ -5,7 +5,7 @@ namespace PlatinumCredentialManager.Api.Dtos;
 // For creating a credential, sending from the user (PUT)
 public record class CreateCredentialDto
 {
-    [Required][StringLength(100)] string serviceName;
-    [StringLength(100)] string? username; // user may not have a username for application
-    [Required][StringLength(100)] string password;
+    [Required][StringLength(100)] string ServiceName;
+    [StringLength(100)] string? Username; // user may not have a username for application
+    [Required][StringLength(100)] string Password;
 }
--- PlatinumCredentialManager.Api/Dtos/UpdateCredentialDto.cs
+++ PlatinumCredentialManager.Api/Dtos/UpdateCredentialDto.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 public record class UpdateCredentialDto
 {
     // Not necessary to update a field
-    [StringLength(100)] string serviceName;
-    [StringLength(100)] string? username; // user may not have a username for application
-    [StringLength(100)] string password;
+    [StringLength(100)] string ServiceName;
+    [StringLength(100)] string? Username; // user may not have a username for application
+    [StringLength(100)] string Password;
 }
