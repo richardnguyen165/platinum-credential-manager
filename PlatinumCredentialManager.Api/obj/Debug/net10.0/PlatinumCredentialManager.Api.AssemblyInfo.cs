61ea624ab4584b8007efedea5c3f48d0f9bb1218
richard.nguyen1 <richard.nguyen2005@outlook.com>
2026-06-27 15:21:48 -0600

    Merged from remote

--- PlatinumCredentialManager.Api/obj/Debug/net10.0/PlatinumCredentialManager.Api.AssemblyInfo.cs
+++ PlatinumCredentialManager.Api/obj/Debug/net10.0/PlatinumCredentialManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatinumCredentialManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f219b188de33c4eae1a50fb2193662658e864d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+738a9f00b41ad48efcc3c9f93263513e9789d0ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatinumCredentialManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatinumCredentialManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
