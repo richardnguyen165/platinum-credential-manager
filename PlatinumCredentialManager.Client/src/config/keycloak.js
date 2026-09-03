05d3d06fd13c1de2da650c730de6a448f7acd649
richardnguyen <richard.nguyen@coredata.ca>
2026-09-03 11:27:42 -0600

    Did some frontend configuration

--- PlatinumCredentialManager.Client/src/config/keycloak.js
+++ PlatinumCredentialManager.Client/src/config/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js'
 
 export const keycloak = new Keycloak({
-    url: 'http://localhost:8080',
-    realm: 'platinum',
-    clientId: 'platinum-vue'
+    url: import.meta.env.VITE_KEYCLOAK_URL ?? 'http://localhost:8080',
+    realm: import.meta.env.VITE_KEYCLOAK_REALM ?? 'platinum',
+    clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID ?? 'platinum-vue'
 });
--- PlatinumCredentialManager.Client/src/main.js
+++ PlatinumCredentialManager.Client/src/main.js
@@ -2,4 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import keycloak from './config/keycloak.js'
 
-createApp(App).mount('#app')
+keycloak.init({
+    onLoad: 'check-sso',
+    silentCheckSsoRedirectUri: window.location.orgin + '/silent-check-sso.html',
+    pkceMethod: 'S256',
+    checkLoginIframe: false
+}).then(() => createApp(App).mount('#app'))
+
+// check-sso: renders the app wether or not user is logged in
+// pkceMethod: 'S256' explictPKCE
+// checkLoginIframe: false, avoids third part cookie flakiness on localhost
