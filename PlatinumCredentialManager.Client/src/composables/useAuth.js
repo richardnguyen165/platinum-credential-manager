04caaca27419332590a96b5be3f4b282c1684c42
richard.nguyen1 <richard.nguyen2005@outlook.com>
2026-09-07 22:13:48 -0600

    Merge branch 'stage-iii-user-operations' of https://github.com/richardnguyen165/platinum-credential-manager into stage-iii-user-operations

--- PlatinumCredentialManager.Client/src/composables/useAuth.js
+++ PlatinumCredentialManager.Client/src/composables/useAuth.js
@@ -1,34 +1,103 @@
 import { reactive } from 'vue'
-import { keycloak } from '../config/keycloak.js'
+import { keycloak } from '@/config/keycloak.js'
+import { urlencoded } from 'express';
 
 const authState = reactive({
-  authenticated: keycloak.authenticated ?? false,
-  username: keycloak.tokenParsed?.preferred_username ?? '',
-})
+  authenticated: false,
+  username:  '',
+  accessToken: '',
+  refreshToken: '',
+  expiresAt: ''
+});
 
-keycloak.onAuthSuccess = () => {
-    authState.authenticated = true
-    authState.username = keycloak.tokenParsed?.preferred_username ?? ''
+const { url, realm, clientId } = keycloak;
+
+const TOKEN_URL = `${url}/realms/${realm}/protocol/openid-connect/token`
+const LOGOUT_URL = `${url}/realms/${realm}/protocol/openid-connect/logout`
+
+async function login(username, password) {
+    let result;
+    try {
+        result = await fetch(TOKEN_URL, { method: 'POST', body: urlencoded({ grant_type: 'password', clientId, username, password }) });
+
+        if (!result.ok) throw new Error('failed');
+
+        const data = await response.json(); // gets payload
+
+        // decode jwt payload
+        const payload = JSON.parse(atob(data.access_token.split('.')[1]))
+
+        // set auth state
+        authState.username = payload.preferred_username
+        authState.accessToken = data.access_token
+        authState.refreshToken = data.refresh_token
+        authState.expiresAt = Date.now() + data.expires_in * 1000
+        authState.authenticated = true;
+
+        return {
+            "status": result.status,
+        }
+    }
+    catch {
+        return {
+            "status": result.status,
+            "error": "Invalid credentials"
+        }
+    }   
 }
 
-keycloak.onAuthLogout = () => {
-    authState.authenticated = false
-    authState.username = ''
+async function refresh() {
+    let result;
+    try {
+        result = await fetch(TOKEN_URL, { method: 'POST', body: urlencoded({ grant_type: 'refresh_token', clientId, username, password }) })
+
+        if (!result.ok) throw new Error('failed')
+
+        const data = await response.json(); // gets payload
+
+        // renews session token
+        authState.refreshToken = data.refresh_token;
+
+        return {
+            "status": result.status,
+        }
+    } 
+    catch {
+        return {
+            "status": result.status,
+            "error": "Failed to refresh token."
+        }
+    }
 }
 
-keycloak.onTokenExpired = () => {
-    keycloak.updateToken(30);
-}
+async function logout() {
+    let result;
+    try {
+        result = await fetch(LOGOUT_URL, { method: 'POST', body: urlencoded({
+            client_id: clientId, refresh_token: authState.refreshToken
+        }) }) 
 
-//  Redirec to keycloak's login page
-function login() {
-    keycloak.login()
-}
+        if (!result.ok) throw new Error('failed')
 
-function logout() {
-    keycloak.logout()
+        // Clear state
+        authState.username = '';
+        authState.authenticated = false;
+        authState.accessToken = '';
+        authState.refreshToken = '';
+        authState.expiresAt = '';
+
+        return {
+            "status": result.status,
+        }
+
+    } catch {
+        return {
+            "status": result.status,
+            "error": "Failed to logout."
+        }
+    }
 }
 
 export default function useAuth() {
-  return { authState, login, logout }
+  return { authState, login, logout, refresh }
 }
--- PlatinumCredentialManager.Client/src/views/LogIn.vue
+++ PlatinumCredentialManager.Client/src/views/LogIn.vue
@@ -1,2 +1,18 @@
 <template>
+  <input v-model="email" />
+  <input v-model="password"/>
+  <button>Log In</button>
 </template>
+
+<script setup>
+    import { ref } from "vue";
+    import axios from "axios";
+    import { authState, login, logout, refresh } from "@/composables/useAuth"
+
+    const email = ref("");
+    const password = ref("");
+
+    function handleSubmit(){
+        login
+    }
+</script>
